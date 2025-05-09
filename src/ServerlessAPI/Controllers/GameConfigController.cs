52d61f39ec05aba2e301dfdb9c4c432f9e0e04b2
jd <jdsanchez2011@gmail.com>
2025-05-08 20:34:49 -0600

    add jwt tokens (game config only)

--- src/ServerlessAPI/Controllers/GameConfigController.cs
+++ src/ServerlessAPI/Controllers/GameConfigController.cs
@@ -1,4 +1,5 @@
 using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ServerlessAPI.Entities;
 using ServerlessAPI.Repositories;
@@ -6,6 +7,7 @@ using ServerlessAPI.Repositories;
 namespace ServerlessAPI.Controllers;
 
 [Route("api/[controller]")]
+[Authorize]
 [ApiController]
 public class GameConfigController : ControllerBase
 {
--- src/ServerlessAPI/Program.cs
+++ src/ServerlessAPI/Program.cs
@@ -4,7 +4,8 @@ using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using ServerlessAPI.Repositories;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,28 @@ builder.Services
             options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         });
+
+var auth0Domain = builder.Configuration["Auth0:Domain"];
+var auth0Audience = builder.Configuration["Auth0:Audience"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{auth0Domain}/";
+        options.Audience = auth0Audience;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = $"https://{auth0Domain}/",
+
+            ValidateAudience = true,
+            ValidAudience = auth0Audience,
+
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
+builder.Services.AddAuthorization();
 
 var useLocalDynamoDb = builder.Configuration["USE_DYNAMODB_LOCAL"] == "true";
 
